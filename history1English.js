1662948b14d4a40d60b649964717593180f28c13
RALBERSME <149826808+RALBERSME@users.noreply.github.com>
2025-05-24 10:45:45 +0200

    English version added

--- history1English.js
+++ history1English.js
@@ -1,39 +1,5 @@
-const historyEvents = [
-    "Ca. 10. Jahrtausend v. Christus Entdeckung der Gegend",
-    "Malereien aus 7300 v. Chr. entdeckt",
-    "Um 200 v. Chr. bereits auf Lamazucht",
-    "Tafi-Kultur (ca. 200 v. Chr. bis 800 n. Chr.) pflanzte Mais an",
-    "Bauernkultur von etwa 1800 v. Chr. bis 500 n. Chr. dominant",
-    "Kultur Santa María (ca. 1200 bis 1470) mmit Terrassenbau",
-    " Im 13. und 14. Jahrhundert expandierte das Inka-Reich stark nach Süden",
-    "Inka-Reicht umfasste um 1450 weite Teile des Nordwestens Argentiniens",
-    "Juan Díaz de Solís erreichte 1516 die Mündung des Río de la Plata",
-    "1526 und 1530 erforschte Sebastiano Caboto den Río Paraná bis zu seinem Oberlauf",
-    "Argentinien wurde von den Spaniern aus drei Richtungen kolonisiert:",
-    "Die erste Siedlung war Sancti Spiritu (1527 -1529)",
-    "Im Jahr 1536 gründete Pedro de Mendoza die Stadt Buenos Aires.",
-    "Von Peru aus nahmen die Spanier den Nordwesten in Besitz,",
-    "und von Westen aus Chile wurde die Region de Cuyo kolonisiert.",
-    "1776 auch das Vizekönigreich Río de la Plata wird vom südlichen Südamerika abgespalten.",
-    "1776 Hauptstadt des neuen Vizekönigreiches wurde Buenos Aires",
-    "1825 Bolivien wird von Argentinien losgelöst",
-    "Juan Manuel de Rosas ab 1829 Gouverneur von Buenos Aires",
-    "Ab 1835 führte ein totalitaristisches, diktatorisches System ein.",
-    "1833 Argentinien verliert die Falklandinseln (Islas Malvinas) an Großbritannien.",
-    " Buenos Aires wurde ab 1880 wieder Hauptstadt der Republik.",
-    "1869 hatte Argentinien 1.836.490 Einwohner, 8 % davon Europäer",
-    "1880 - 1929 wirtschaftlicher Aufschwung und verstärkte Einwanderung aus Europa.",
-    "1930 wurde Yrigoyen bei einem Militärputsch gestürzt.",
-    "Ramón Castillo wurde 1943 durch einen Putsch entmachtet",
-    "Übergangsphase bis 1946, in der das Militär die Macht innehatte",
-    "Juan Perón trickst sich 1945 an die Macht",
-    "Perón wird am 10. Oktober 1945 entlassen und verhaftet",
-    "Juan Perón gewann die Wahlen 1946", "Perón dominierte mit seiner Frau Eva Perón (genannt Evita, † 1952) das politische Leben bis 1955",
-    "Seine Regierungszeit war eine Mischung aus Demokratie und Diktatur",
-    "Bis in die 1950er Jahre war Argentinien wohlhabender als die europäischen Länder.",
-    "Ende dieser Rolle - mit dem continue button geht`s weiter"    
-  ];
-  const historyEventsEnglish = ["Circa 10th millennium BC: Discovery of the area",
+
+  const historyEvents = ["Circa 10th millennium BC: Discovery of the area",
 "Paintings from 7300 BC discovered",
 "Llama breeding was already taking place around 200 BC",
 "Tafi culture (c. 200 BC to 800 AD) cultivated maize",
@@ -71,10 +37,6 @@ const historyEvents = [
   let countNumber = 0; 
   let activeIndex = 0;
   const rotate = -360 / events.length;
-
-  function setEnglish(){
-    window.location.href= 'history1English.html'
-   }
 
   init();  
   
