45ea0170ad6af7908aa5ae0b43467d2c519b2d77
RALBERSME <149826808+RALBERSME@users.noreply.github.com>
2025-05-25 12:36:02 +0200

    upload Tierra Del Fuego page

---
+++ tierraDelFuego.js
@@ -0,0 +1,18 @@
+function nextGoal() {
+    if (localStorage.getItem("argentina")) {
+        const reisezieleArray = JSON.parse(localStorage.getItem("argentina"));
+        if(reisezieleArray.length > 1) {
+            reisezieleArray.shift()
+            localStorage.setItem("argentina", JSON.stringify(reisezieleArray));
+            const newReisezieleArray = JSON.parse(localStorage.getItem("argentina"));
+            if (newReisezieleArray[0] != undefined) {
+            window.location.href = `${newReisezieleArray[0]}.html`}
+        } else {
+             window.location.href = "bye.html"; 
+        }
+    } else {
+        window.location.href = "bye.html"; 
+    }  
+
+}
+
