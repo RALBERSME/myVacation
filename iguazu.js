8b1dfd98097351b6673b8bc34c107f1b7217342e
RALBERSME <149826808+RALBERSME@users.noreply.github.com>
2025-05-25 13:11:20 +0200

    update undefined issue

--- iguazu.js
+++ iguazu.js
@@ -11,16 +11,13 @@ function nextGoal() {
             reisezieleArray.shift()
             localStorage.setItem("argentina", JSON.stringify(reisezieleArray));
             const newReisezieleArray = JSON.parse(localStorage.getItem("argentina"));
-            console.log("reisezielArray", newReisezieleArray)
-            window.location.href = `${newReisezieleArray[0]}.html`
+            if (newReisezieleArray[0] != undefined) {
+            window.location.href = `${newReisezieleArray[0]}.html`}
         } else {
              window.location.href = "bye.html"; 
         }
     } else {
         window.location.href = "bye.html"; 
-    }
-    
-    
-   
+    }  
 
 }
